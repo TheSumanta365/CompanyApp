86684247b65aeab9d1249b06dc69507c6a98f02d
TheSumanta365 <imsumanta10@gmail.com>
2025-02-11 21:21:45 +0530

    Make username check case-insensitive

--- login.py
+++ login.py
@@ -1,7 +1,6 @@
 def login(username, password):
-    if username == "admin" and password == "admin123":
+    if username.lower() == "admin" and password == "admin123":
         print("Login Successful")
     else:
         print("Invalid Credentials")
 
-login("admin", "admin123")
